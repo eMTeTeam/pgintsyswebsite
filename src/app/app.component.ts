4fc1c7f6d78bc648022ef33a7f8066600d623503
Mogan <moganvig90@gmail.com>
2020-07-15 18:07:13 +0530

    new checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'website';
+
+
+  ngOnInit() {
+    AOS.init({ duration: 1000, once: true });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll = ($event: any) => {
+    const verticalOffset = window.pageYOffset
+      || document.documentElement.scrollTop
+      || document.body.scrollTop || 0;
+
+    if (verticalOffset > 90) {
+      document.getElementById("mainNav").classList.add('navbar-shrink');
+    } else {
+      document.getElementById("mainNav").classList.remove('navbar-shrink');
+    }
+  }
 }
