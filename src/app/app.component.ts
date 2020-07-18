d39bd79722b3464d8858cb0bf2ae94c1958e7f6e
Mogan <moganvig90@gmail.com>
2020-07-18 22:04:12 +0530

    new scroll animation update and scroll to Top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import * as AOS from 'aos';
+import jump from 'jump.js';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,11 @@ import * as AOS from 'aos';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'website';
+  title = 'PG InitSys';
+  ariaExpanded: boolean = false;
+  windowScrolled: boolean = false;
 
+  constructor() { }
 
   ngOnInit() {
     AOS.init({ duration: 1000, once: true });
@@ -16,14 +20,39 @@ export class AppComponent implements OnInit {
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll = ($event: any) => {
-    const verticalOffset = window.pageYOffset
-      || document.documentElement.scrollTop
-      || document.body.scrollTop || 0;
+    const verticalOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-    if (verticalOffset > 90) {
+    if (verticalOffset > 100) {
       document.getElementById("mainNav").classList.add('navbar-shrink');
-    } else {
+      this.windowScrolled = true;
+    } else if (this.windowScrolled && verticalOffset < 10) {
+      this.windowScrolled = false;
       document.getElementById("mainNav").classList.remove('navbar-shrink');
     }
+  }
+
+  clickMenu = () => {
+    this.ariaExpanded = !this.ariaExpanded;
+  }
+
+  scrollToTarget = (target: string) => {
+    const menuButtonElement = document.getElementById("menu-button");
+    menuButtonElement.classList.add('collapsed');
+    menuButtonElement.setAttribute("aria-expanded", "false");
+    document.getElementById("navbarMenuContent").classList.remove('show');
+
+    let offset = -10;
+    if (target === '.careerTarget') {
+      offset = -50;
+    }
+
+    this.ariaExpanded = false;
+    jump(target, {
+      offset: offset
+    });
+  }
+
+  scrollToTop = () => {
+    jump('.headerTarget');
   }
 }
