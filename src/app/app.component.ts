0c81d3994edace1db034347ac5f3dd0bf446ad39
Sathya <sathyanan29@gmail.com>
2020-09-12 22:09:46 +0530

    compressed images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,29 +64,29 @@ export class AppComponent implements OnInit {
 
   loadServices = () => {
     this.services = [
-      { icon: 'ion-ios-world-outline', title: 'Mobile & Web Development', caption: 'Mobile & Web Development' },
-      { icon: 'ion-ios-paper-outline', title: 'Data Science', caption: 'Data Science' },
-      { icon: 'ion-ios-grid-view-outline', title: 'IOT (Rename Embed)', caption: 'IOT (Rename Embed)' },
-      { icon: 'ion-ios-cloud-outline', title: 'Cloud Computing', caption: 'Cloud Computing' },
-      { icon: 'ion-ios-locked-outline', title: 'Cybersecurity & Block chain', caption: 'Cybersecurity & Block chain' },
-      { icon: 'ion-ios-color-wand-outline', title: 'Digital Transformation IR4.0', caption: 'Digital Transformation IR4.0' },
+      { icon: 'ion-ios-world-outline', title: 'Mobile & Web Development', caption: 'Native & Hybrid Application development' },
+      { icon: 'ion-ios-paper-outline', title: 'Data Science', caption: 'Data mining, machine learning and big data' },
+      { icon: 'ion-ios-grid-view-outline', title: 'IOT', caption: 'Embedded with sensors and exchange data' },
+      { icon: 'ion-ios-cloud-outline', title: 'Cloud Computing', caption: 'Data centers available to many users over the Internet.' },
+      { icon: 'ion-ios-locked-outline', title: 'Cybersecurity & Block chain', caption: 'High level security for any software over internet' },
+      { icon: 'ion-ios-color-wand-outline', title: 'Digital Transformation IR4.0', caption: 'Data exchange in manufacturing technologies' },
       { icon: 'ion-ios-barcode-outline', title: 'Information Security Management', caption: 'Information Security Management' },
-      { icon: 'ion-ios-speedometer-outline', title: 'Cost Reduction & Process Improvement', caption: 'Cost Reduction & Process Improvement' },
-      { icon: 'ion-ios-navigate-outline', title: 'Robot and Drone', caption: 'Robot and Drone' }
+      { icon: 'ion-ios-speedometer-outline', title: 'Cost Reduction & Process Improvement', caption: 'Make your organization more effective' },
+      { icon: 'ion-ios-navigate-outline', title: 'Robot and Drone', caption: 'Metaphorical software robots or on artificial intelligence /digital workers' }
     ];
   }
 
   loadTrainings = () => {
     this.trainings = [
-      { icon: 'ion-ios-analytics-outline', title: 'IR4.0 opportunities and challenges', caption: 'IR4.0 opportunities and challenges' },
-      { icon: 'ion-ios-analytics-outline', title: 'IOT & Robotic System', caption: 'IOT & Robotic System' },
-      { icon: 'ion-ios-analytics-outline', title: 'Program Logic Controller', caption: 'Program Logic Controller' },
-      { icon: 'ion-ios-analytics-outline', title: 'Arduino/ Raspberry PI', caption: 'Arduino/ Raspberry PI' },
-      { icon: 'ion-ios-analytics-outline', title: '3D Design and Printer', caption: '3D Design and Printer' },
-      { icon: 'ion-ios-analytics-outline', title: 'AI & Python', caption: 'AI & Python' },
-      { icon: 'ion-ios-analytics-outline', title: 'Java Development', caption: 'Java Development' },
-      { icon: 'ion-ios-analytics-outline', title: 'Lean Six Sigma', caption: 'Lean Six Sigma' },
-      { icon: 'ion-ios-analytics-outline', title: 'Marketing, Leadership etc ', caption: 'Marketing, Leadership etc ' }
+      { icon: 'ion-ios-book-outline', title: 'IR4.0 opportunities and challenges', caption: 'IR4.0 opportunities and challenges' },
+      { icon: 'ion-ios-book-outline', title: 'IOT & Robotic System', caption: 'IOT & Robotic System' },
+      { icon: 'ion-ios-book-outline', title: 'Program Logic Controller', caption: 'Program Logic Controller' },
+      { icon: 'ion-ios-book-outline', title: 'Arduino/ Raspberry PI', caption: 'Arduino/ Raspberry PI' },
+      { icon: 'ion-ios-book-outline', title: '3D Design and Printer', caption: '3D Design and Printer' },
+      { icon: 'ion-ios-book-outline', title: 'AI & Python', caption: 'AI & Python' },
+      { icon: 'ion-ios-book-outline', title: 'Java Development', caption: 'Java Development' },
+      { icon: 'ion-ios-book-outline', title: 'Lean Six Sigma', caption: 'Lean Six Sigma' },
+      { icon: 'ion-ios-book-outline', title: 'Marketing, Leadership etc ', caption: 'Marketing, Leadership etc ' }
     ];
   }
 
