07c78a3e1dff6e77826f7e8aa1121c083896c802
Mogan <moganvig90@gmail.com>
2020-09-12 13:44:09 +0530

    latest update with feedback included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,13 @@ export class AppComponent implements OnInit {
   ariaExpanded: boolean = false;
   windowScrolled: boolean = false;
 
-  constructor() { }
+  services: Array<any> = [];
+  trainings: Array<any> = [];
+
+  constructor() {
+    this.loadServices();
+    this.loadTrainings();
+  }
 
   ngOnInit() {
     AOS.init({ duration: 1000, once: true });
@@ -55,4 +61,33 @@ export class AppComponent implements OnInit {
   scrollToTop = () => {
     jump('.headerTarget');
   }
+
+  loadServices = () => {
+    this.services = [
+      { icon: 'ion-ios-world-outline', title: 'Mobile & Web Development', caption: 'Mobile & Web Development' },
+      { icon: 'ion-ios-paper-outline', title: 'Data Science', caption: 'Data Science' },
+      { icon: 'ion-ios-grid-view-outline', title: 'IOT (Rename Embed)', caption: 'IOT (Rename Embed)' },
+      { icon: 'ion-ios-cloud-outline', title: 'Cloud Computing', caption: 'Cloud Computing' },
+      { icon: 'ion-ios-locked-outline', title: 'Cybersecurity & Block chain', caption: 'Cybersecurity & Block chain' },
+      { icon: 'ion-ios-color-wand-outline', title: 'Digital Transformation IR4.0', caption: 'Digital Transformation IR4.0' },
+      { icon: 'ion-ios-barcode-outline', title: 'Information Security Management', caption: 'Information Security Management' },
+      { icon: 'ion-ios-speedometer-outline', title: 'Cost Reduction & Process Improvement', caption: 'Cost Reduction & Process Improvement' },
+      { icon: 'ion-ios-navigate-outline', title: 'Robot and Drone', caption: 'Robot and Drone' }
+    ];
+  }
+
+  loadTrainings = () => {
+    this.trainings = [
+      { icon: 'ion-ios-analytics-outline', title: 'IR4.0 opportunities and challenges', caption: 'IR4.0 opportunities and challenges' },
+      { icon: 'ion-ios-analytics-outline', title: 'IOT & Robotic System', caption: 'IOT & Robotic System' },
+      { icon: 'ion-ios-analytics-outline', title: 'Program Logic Controller', caption: 'Program Logic Controller' },
+      { icon: 'ion-ios-analytics-outline', title: 'Arduino/ Raspberry PI', caption: 'Arduino/ Raspberry PI' },
+      { icon: 'ion-ios-analytics-outline', title: '3D Design and Printer', caption: '3D Design and Printer' },
+      { icon: 'ion-ios-analytics-outline', title: 'AI & Python', caption: 'AI & Python' },
+      { icon: 'ion-ios-analytics-outline', title: 'Java Development', caption: 'Java Development' },
+      { icon: 'ion-ios-analytics-outline', title: 'Lean Six Sigma', caption: 'Lean Six Sigma' },
+      { icon: 'ion-ios-analytics-outline', title: 'Marketing, Leadership etc ', caption: 'Marketing, Leadership etc ' }
+    ];
+  }
+
 }
